53af371b79965b27c5f50454dcd9659b1259dd66
Francisco <frangarcia38@hotmail.com>
2022-08-14 18:54:03 -0600

    Se añade archivo script.sql que contendra las tablas del proyecto

---
+++ script.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE mobilecheck
+
+CREATE TABLE Usuario(
+    id serial primary key,
+    nombre varchar(50),
+    apellido varchar(50),
+    telefono varchar(50),
+    email varchar(50),
+    activo bool,
+    usuario varchar(50),
+    password text,
+    web bool,
+    movil bool
+)
