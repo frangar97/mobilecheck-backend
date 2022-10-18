dcc7312b8dd4a844c75f147b23b8f79d43e1fed3
Francisco A. Garcia <frangarcia38@hotmail.com>
2022-10-18 10:41:47 -0600

    correccion cors

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -9,4 +9,11 @@ func corsMiddleware(c *gin.Context) {
 	c.Header("Access-Control-Allow-Methods", "*")
 	c.Header("Access-Control-Allow-Headers", "*")
 	c.Header("Content-Type", "application/json")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(204)
+		return
+	}
+
+	c.Next()
 }
